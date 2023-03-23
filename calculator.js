b7cf4dc68d76ebaab2ad1c5b91a7a181430479c1
Mohit Punia <97046203+MOHIT-0001@users.noreply.github.com>
2023-03-23 14:48:28 +0530

    updated

--- calculator.js
+++ calculator.js
@@ -15,20 +15,48 @@ buttons.forEach(function (button) {
         document.querySelector('.screen').value += value
         let presentValue = document.querySelector('.screen').value
         let checkingRepatedOperator = presentValue[presentValue.length - 1]
-        let lastShowingThing=presentValue[presentValue.length - 1]
-        let secondLastShowingThing=presentValue[presentValue.length - 2]
+        let lastShowingThing = presentValue[presentValue.length - 1]
+        let secondLastShowingThing = presentValue[presentValue.length - 2]
         console.log(lastShowingThing)
         if (value !== '=') {
             if (presentValue.length - 1 !== 0) {
                 console.log(presentValue[presentValue.length - 1])
-                if(lastShowingThing==='.'){  // in between . than 0.
-                    if(['/','*','+','-'].includes(presentValue[presentValue.length - 2])){
-                        // document.querySelector('.screen').value
-                        //write code to insert 0 in second last place of string
+                if (lastShowingThing === '.') {  // in between . than 0.
+                    if (['/', '*', '+', '-'].includes(presentValue[presentValue.length - 2])) {
+                        let newInput = document.querySelector('.screen').value.slice(0, -1)     //write code to insert 0 in second last place of string
+                        document.querySelector('.screen').value = newInput + "0."
                     }
-                    if(presentValue[presentValue.length - 2]==='.'){
+                    // if (typeof presentValue[presentValue.length - 2] === 'number') {
+
+                    // }
+                    if (presentValue[presentValue.length - 2] === '.') {
                         alert('invalid number')
-                        //write code to remove last . decimal
+                        screen.value = screen.value.slice(0, -1)                                  // remove last . decimal from ..
+                    }
+                    if (presentValue.includes('+') || presentValue.includes('-') || presentValue.includes('*') || presentValue.includes('/')) {
+                        let string = ''
+                        for (let i = presentValue.length - 2; i >= 0; i--) {
+                            if (presentValue[i] >= 0 || presentValue <= 9 || presentValue[i]=='.') {
+                                string = string + presentValue[i]
+                            }
+                            else {
+                                break                                              // remove second decimal from  5*4.4+5.5.
+                            }
+                        }
+                        console.log(string)
+                       if(string.includes('.')){
+                        alert('invalid number')
+                        screen.value = screen.value.slice(0, -1)
+                       }
+                    }
+                    else {
+                        console.log("hello")
+                        let numberWithoutFirstDecimal = screen.value.slice(0, -1)
+                       if(numberWithoutFirstDecimal.includes('.')){
+                        alert('invalid number')
+                        screen.value=screen.value.slice(0, -1)
+                       }                                                         //  remove second decimal from 5.5.
+                                                                                          
                     }
                 }
                 if (checkingRepatedOperator === '/'
@@ -36,33 +64,45 @@ buttons.forEach(function (button) {
                     || checkingRepatedOperator === '+'
                     || checkingRepatedOperator === '-') {
                     if (!(presentValue[presentValue.length - 2] >= 0
-                        && presentValue[presentValue.length - 2] <= 9)) {
-                        alert('Two operator continous')
+                        && presentValue[presentValue.length - 2] <= 9) && !(presentValue[presentValue.length - 2] === '.')) {
+                        alert('Two operator continous')                                 // to stop +/ but ./ not stop
                         let showAfterReaptedOperator = presentValue.slice(0, -1)
                         document.querySelector('.screen').value = showAfterReaptedOperator
                     }
                 }
             }
-            else{   //if first time . than it will be 0.
-                if(lastShowingThing==='.'){
-                      // write to insert o in front of .   , in first letters of question
-                }      
+            else {   //if first time . than it will be 0.
+                if (lastShowingThing === '.') {
+                    document.querySelector('.screen').value = "0."     // write to insert o in front of .   , in first letters of question
+                }
             }
         }
         if (value === "=") {
-            let checkingLastOperatorWithoutNumber = presentValue[presentValue.length - 2]
-            if (checkingLastOperatorWithoutNumber === '/'
-                || checkingLastOperatorWithoutNumber === '*'
-                || checkingLastOperatorWithoutNumber === '+'
-                || checkingLastOperatorWithoutNumber === '-') {
-                alert('enter valid number')  //checking if just after operator no equal sign
-                document.querySelector('.screen').value = ''
+            if (presentValue.length - 1 === 0) {
+                document.querySelector('.screen').value = ''    //if first value is = than empty input field //stoping====
             }
             else {
-                let solveValue = presentValue.slice(0, -1)
-                console.log(solveValue)
-                startEvaluation(solveValue)
+                if (lastShowingThing === '=' && !(isNaN(presentValue.slice(0, -1)))) {
+                    document.querySelector('.screen').value = document.querySelector('.screen').value.slice(0, -1)  //stoping first letter 8==
+                }
+                else {
+                    let checkingLastOperatorWithoutNumber = presentValue[presentValue.length - 2]
+                    if (checkingLastOperatorWithoutNumber === '/'
+                        || checkingLastOperatorWithoutNumber === '*'
+                        || checkingLastOperatorWithoutNumber === '+'
+                        || checkingLastOperatorWithoutNumber === '-'
+                    ) {
+                        alert('enter valid number')  //checking if just after operator no equal sign
+                        document.querySelector('.screen').value = ''
+                    }
+                    else {
+                        let solveValue = presentValue.slice(0, -1)
+                        console.log(solveValue)
+                        startEvaluation(solveValue)
+                    }
+                }
             }
+
         }
     })
 });
@@ -75,12 +115,12 @@ function startEvaluation(equation) {
     let operandString = ""
 
     for (let index = 0, operatorIndex = 0, bracketIndex = 0; index < equation.length; index++) {
-       
+
         if (equation.charAt(index) == '+'
             || equation.charAt(index) == '-'
             || equation.charAt(index) == '*'
             || equation.charAt(index) == '/') {
-          
+
             operators[operatorIndex++] = equation.charAt(index)
             operandString += " "
         }
@@ -88,15 +128,15 @@ function startEvaluation(equation) {
             operandString += equation.charAt(index)
         }
     }
-    
+
     operandsTemp = operandString.split(" ")
-console.log(operandsTemp)  // in string here in array
-console.log(operators)    // array operator in string
+    console.log(operandsTemp)  // in string here in array
+    console.log(operators)    // array operator in string
 
     // Storing operands into operands[] array
     for (var index = 0, indexOperand = 0; index < operators.length; index++) {
-            operands[index] = Number(operandsTemp[indexOperand++])
-            console.log(operands)
+        operands[index] = Number(operandsTemp[indexOperand++])
+        console.log(operands)
     }
     operands[operands.length] = Number(operandsTemp[indexOperand])
     console.log(operands)   //in number here in array
@@ -177,7 +217,7 @@ function evaluate(indexStart, indexEnd, operators, operands) {
 
     // Next evaluating ADDITION and SUBSTRACTION
     for (let index = indexStart; index < indexEnd; index++) {
-console.log(index)
+        console.log(index)
         let indexFirstOperand = index
         let indexNextOperand = index + 1
         let indexNextOperator = index
@@ -217,7 +257,7 @@ console.log(index)
         }
         // SUBSTRACTION
         else if (operators[indexNextOperator] == '-') {
-            
+
             result = operands[indexFirstOperand] - operands[indexNextOperand]
             operands[indexNextOperand] = result
             operands[indexFirstOperand] = null
